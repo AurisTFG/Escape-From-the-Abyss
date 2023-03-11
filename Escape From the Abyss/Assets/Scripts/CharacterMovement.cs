ee0501f05bcc87ba6f1b27ca19fb1e2ac56dbe11
Shoffen <61907814+Shoffen@users.noreply.github.com>
2023-03-11 21:23:42 +0200

    pridejau komentara
    fshsf

--- Escape From the Abyss/Assets/Scripts/CharacterMovement.cs
+++ Escape From the Abyss/Assets/Scripts/CharacterMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class CharacterMovement : MonoBehaviour
 {
+    //rambyno
     //private string fpsText;
     //public float deltaTime;
     //-------------------------------------------------------------------------------------------------
