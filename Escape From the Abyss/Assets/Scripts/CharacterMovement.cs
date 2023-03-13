cb87f72cf2470645715671e5192a992128d2d8d6
Shoffen <61907814+Shoffen@users.noreply.github.com>
2023-03-13 19:28:08 +0200

    Animations, Movement
    Pridėjau animacijas, characterį.

--- Escape From the Abyss/Assets/Scripts/CharacterMovement.cs
+++ Escape From the Abyss/Assets/Scripts/CharacterMovement.cs
@@ -1,52 +1,80 @@
 using UnityEngine;
-
+using System.Collections.Generic;
 public class CharacterMovement : MonoBehaviour
 {
-    public float speed;
-    public Vector3 change;
+    //--------------------------------------------------------------------------------------------------
+    public Animator playerAnim; 
     private Rigidbody myRigidbody;
-
+    //--------------------------------------------------------------------------------------------------
+    private float angle;
+    public float speed;
+    public float turnSpeed;
+    public Vector3 change;
+    private Quaternion targetRotation;
+    private Quaternion fixedQ = new Quaternion(0, 0, 0, 1);
+    private Quaternion lastRotation;
+    public Stack<KeyCode> listed_buttons;
+    //--------------------------------------------------------------------------------------------------
     void Start()
     {
+        
         myRigidbody = GetComponent<Rigidbody>();
     }
     void FixedUpdate()
     {
+
+        if (playerAnim.GetBool("walking"))
+        {
+            lastRotation = myRigidbody.transform.rotation;
+        }
+        else
+        {
+            myRigidbody.transform.rotation = lastRotation;
+        }    
+  
+        CalculateDirection();
+        Rotate();
         GetInput();
         UpdateMovement();
     }
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(change.x, change.z);
+        angle *= Mathf.Rad2Deg;
+    }
+
+    void Rotate()
+    {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
     void UpdateMovement()
     {
-        if (Mathf.Abs(change.x) == 1 && Mathf.Abs(change.z) == 1) // eina istrizai
-            change *= 0.707f;
-
-        myRigidbody.MovePosition(myRigidbody.position + change * speed * Time.fixedDeltaTime);  
+        if (change != Vector3.zero)
+        {
+            if (Mathf.Abs(change.x) == 1 && Mathf.Abs(change.z) == 1) // eina istrizai
+            {
+                change *= 0.707f;
+            }
+            playerAnim.SetBool("walking", true);
+            myRigidbody.MovePosition(myRigidbody.position + change * speed * Time.fixedDeltaTime);
+        }
+       
     }
     void GetInput()
     {
-        change = Vector3.zero;
 
+
+        change = Vector3.zero;
+        playerAnim.SetBool("walking", false);
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            change.x = 1;
+            change.z = 1;
+            
+            
         }
             
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-        {
-            if (change.x == 1)
-            {
-                change = Vector3.zero;
-                return;
-            }
-            change.x = -1;
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            change.z = 1;
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             if (change.z == 1)
             {
@@ -54,6 +82,69 @@ public class CharacterMovement : MonoBehaviour
                 return;
             }
             change.z = -1;
-        }      
+            
+           
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            change.x = -1;
+           
+            
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+           
+            if (change.x == -1)
+            {
+                change = Vector3.zero;
+                return;
+            }
+            change.x = 1;
+           
+            
+        }
+        /*if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            listed_buttons.Push(KeyCode.UpArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            listed_buttons.Push(KeyCode.DownArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            listed_buttons.Push(KeyCode.RightArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            listed_buttons.Push(KeyCode.LeftArrow);
+        }
+
+        
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            //listed_buttons.Peek();
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            //listed_buttons.Push(KeyCode.DownArrow);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            if(listed_buttons.Peek() == KeyCode.RightArrow)
+            {
+                listed_buttons.Pop()
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+           // listed_buttons.Remove(KeyCode.LeftArrow);
+        }*/
+
     }
+    
+
+
 }
--- Escape From the Abyss/Assets/Scripts/FollowPlayer.cs
+++ Escape From the Abyss/Assets/Scripts/FollowPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public Transform target;
+    /*public Transform target;
     public float smoothSpeed = 0.125f;
     public Vector3 locationOffset;
     public Vector3 rotationOffset;
@@ -18,5 +18,18 @@ public class FollowPlayer : MonoBehaviour
         Quaternion desiredrotation = target.rotation * Quaternion.Euler(rotationOffset);
         Quaternion smoothedrotation = Quaternion.Lerp(transform.rotation, desiredrotation, smoothSpeed);
         transform.rotation = smoothedrotation;
+    }*/
+    public Transform target;
+    public Vector3 velocity = Vector3.zero;
+    public float smoothSpeed = 0.05f;
+    public Vector3 offset = new Vector3(6, 6, -6);
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
     }
 }
