a739f1d666af1c6ccca3b8a2aac5a15935e2afa8
Nojuzas <127687428+Nojuzas@users.noreply.github.com>
2023-03-16 05:22:03 +0200

    combat update
    combo ataka žaidėjui (kol animacijoj jei paspaudi dar kartą ataka, combo bus), detectina, jei į priešą pataikei, priešo modelis ir animacijos

--- Escape From the Abyss/Assets/Scripts/EnemyAI.cs
+++ Escape From the Abyss/Assets/Scripts/EnemyAI.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Interfaces;
 
 
-public class EnemyAI : MonoBehaviour
+public class EnemyAI : MonoBehaviour, IDamageable
 {
+    public Animator enemyAnim;
 
     public float lookRadius = 10f;
 
@@ -16,15 +18,18 @@ public class EnemyAI : MonoBehaviour
     {
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+        enemyAnim = GetComponent<Animator>();
 
 
     }
 
     void Update()
     {
+        enemyAnim.SetBool("walking", false);
         float distance = Vector3.Distance(target.position, transform.position);
         if (distance <= lookRadius)
         {
+            enemyAnim.SetBool("walking", true);
             agent.SetDestination(target.position);
             if (distance <= agent.stoppingDistance)
             {
@@ -36,6 +41,7 @@ public class EnemyAI : MonoBehaviour
 
     void FaceTarget()
     {
+        enemyAnim.SetBool("walking", false);
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
@@ -47,4 +53,10 @@ public class EnemyAI : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 
+    public void Damage(int damageAmount)
+    {
+        Debug.Log("IKIRTAU TAU"); //temp
+        enemyAnim.SetTrigger("hit");
+        
+    }
 }
