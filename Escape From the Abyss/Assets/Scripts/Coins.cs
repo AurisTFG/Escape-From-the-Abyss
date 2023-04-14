b2c5cd6340e311e7b2177a00a97be28dc4f36f25
Shoffen <61907814+Shoffen@users.noreply.github.com>
2023-04-14 18:52:03 +0300

    Coins animacijos ir garsas
    Pridėjau coinso paėmimo animacijas, garso efektą bei patobulinau statistikos UI, pasidarydamas ,,Canvas", kurį vėliau galėsiu plėsti iki tiek, kiek reikalaus žaidimas, tai yra: Jame galės būt tiek gyvybės, tiek energija, tiek turimas coinsų kiekis bei įvairios nuotraukos.

--- Escape From the Abyss/Assets/Scripts/Coins.cs
+++ Escape From the Abyss/Assets/Scripts/Coins.cs
@@ -1,31 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Coins : MonoBehaviour
 {
-   
+    public AudioSource collectSound;
+    public TMP_Text coinDisplay;
     public int value;
     public float angle;
+
+    public float speed;
+
+
+    bool moveCoin;
+    GameObject target;
     
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("toCoins");
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (moveCoin)
+        {
+            Debug.Log("TELEPORTUOJAM");
+            transform.position = Vector3.Lerp(transform.position, target.transform.position, speed * Time.fixedDeltaTime);
+        }
         
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("PRIDEDAM PINIGU");
-            Destroy(gameObject);
+            //Debug.Log("PRIDEDAM PINIGU");
+            collectSound.Play();
+            gameObject.GetComponent<CapsuleCollider>().enabled = false;
+            moveCoin = true;
+            Destroy(gameObject, 2f);
             CoinsCounter.instance.IncreaseCoins(value);
         }
     }
--- Escape From the Abyss/Assets/Scripts/RotateCoin.cs
+++ Escape From the Abyss/Assets/Scripts/RotateCoin.cs
@@ -7,9 +7,10 @@ public class RotateCoin : MonoBehaviour
     Rigidbody rigidbody;
     public float rotateSpeed;
     public Vector3 change;
-    public int moveSpeed;
+    public float moveSpeed;
     private bool down = true;
     private bool up = false;
+
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -31,11 +32,11 @@ public class RotateCoin : MonoBehaviour
         }
         if(down)
         {
-            change.y -= 0.01f;
+            change.y -= 0.01f*moveSpeed;
         }
         if(up)
         {
-            change.y += 0.01f;
+            change.y += 0.01f*moveSpeed;
         }
 
         rigidbody.MovePosition(change);
