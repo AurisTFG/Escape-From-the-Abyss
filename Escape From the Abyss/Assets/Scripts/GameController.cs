8ad5c798a07f0d404a3356c59392d48a71235717
Shoffen <61907814+Shoffen@users.noreply.github.com>
2023-05-15 14:14:15 +0300

    ExplodeSutaisymas
    .

--- Escape From the Abyss/Assets/Scripts/GameController.cs
+++ Escape From the Abyss/Assets/Scripts/GameController.cs
@@ -5,7 +5,19 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameOverScreen GameOverScreen;
+    public GameObject barrel;
+    public GameObject propan;
+    public GameObject spawn;
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            GameObject instantiatedBarrel = Instantiate(barrel, spawn.transform.position, new Quaternion(-90, 0, 0, 0));
+            Vector3 eulerRotation = instantiatedBarrel.transform.rotation.eulerAngles;
+            Debug.Log("Barrel rotation: " + eulerRotation);
 
+        }
+    }
     public void GameOver()
     {
         GameOverScreen.Setup();
