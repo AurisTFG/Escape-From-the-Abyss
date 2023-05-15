503e93aa2e3ab865891b39240d6c86a9e2955638
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2023-05-15 20:48:34 +0300

    test

--- Escape From the Abyss/Assets/Scripts/HealthBar.cs
+++ Escape From the Abyss/Assets/Scripts/HealthBar.cs
@@ -8,14 +8,12 @@ public class HealthBar : MonoBehaviour
 
     public Slider slider;
 
-    // Start is called before the first frame update
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
     }
 
-    // Update is called once per frame
     public void SetHealth(int health)
     {
         slider.value = health;
